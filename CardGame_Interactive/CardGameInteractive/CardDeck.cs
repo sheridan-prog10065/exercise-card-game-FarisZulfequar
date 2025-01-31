2d44e90bffa102f355c4bd67e7e83df8ddb1efdc
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2025-01-31 14:39:58 -0500

    Defined all field variables in all the user defined types

---
+++ CardGame_Interactive/CardGameInteractive/CardDeck.cs
@@ -0,0 +1,13 @@
+namespace CardGameInteractive;
+
+/// <summary>
+/// Defines the card deck as a list of cards
+/// </summary>
+public class CardDeck
+{
+    // This is where the filed variables will be declared
+    /// <summary>
+    /// The list of cards in the deck
+    /// </summary>
+    private List<Card> _cardList;
+}        
